9565f9f6867256d3a7f82283a3b4714047f148f4
mostafa hosny <mostafa.hosny@otlob.com>
2018-04-06 18:41:16 +0200

    done with testing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ApplicationRecord
+  #-------------validations ----------------
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  # we used allow_blank to avoid getting multiple error messages
+  # when the field is blank.
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    messgae: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
